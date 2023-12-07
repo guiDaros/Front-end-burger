8f297e8351d9816bd4cfe3d6dd2c6fe0125ea42f
guiDaros <guivdaros@gmail.com>
2023-12-07 10:34:16 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCodeBurger = axios.create({
-    baseURL: 'http://localhost:3011/'
+    baseURL: 'https://backend-burger-production.up.railway.app/'
 });
 
 apiCodeBurger.interceptors.request.use(async (config) => {
